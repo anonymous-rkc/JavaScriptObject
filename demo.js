7907eb10a983c1a4684b2622c337a8de77869b31
Rahul Kumar <36847653+anonymous-rkc@users.noreply.github.com>
2020-04-13 18:53:53 +0530

    inspecting objects property

--- demo.js
+++ demo.js
@@ -10,4 +10,16 @@
 
 registerUser("Anonynous-","rkc");
 
+var person = { 
+  firstName:"Anonymous",
+  lastName:"rkc",
+  age:17,
+  isAdult() {return this.age >= 18}
+};
+display(person.isAdult());
+display(Object.keys(person));
+for(var x in person){
+  display(x + " " +person[x]);
+}
+
 })();
